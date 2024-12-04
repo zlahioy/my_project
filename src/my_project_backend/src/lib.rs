ee7e73f6964e13c6962931e1100b0750d25b8f20
zlahioy . <michalszczepan84@gmail.com>
2024-12-04 09:39:53 +0000

    added option to send msg

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,21 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| {
+        msg.borrow_mut() = new_msg;
+    })
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|msg| msg.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
